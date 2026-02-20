f98dbad8089cfc18aaf39a58131067a6e8ba134a
edu-alasos <alansebastian.soszynski@skola.taby.se>
2026-02-20 13:30:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const text = [
+    svenska = [
+        rubrik = [],
+        mainText = []
+    ],
+
+    engelska = [
+        rubrik = [],
+        mainText = []
+    ]
+]
+
+document.addEventListener('DOMContentLoaded', () => { // äfter sidan har laddats
+
+    const icon = document.querySelector('.icon');
+    icon.addEventListener('click', () => { // när flagga ikonen trycks händer detta
+        
+    });
+
+});
